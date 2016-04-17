948580042529a8c515792771a8177b6b03ba45b0
Nikhil <nbagde1@binghamton.edu>
2016-04-17 02:26:53 -0400

    Comments added

--- src/Driver.java
+++ src/Driver.java
@@ -16,14 +16,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Driver {
     public static void main(String args[]) throws IOException {
+
         FileProcessor inputProcessor = new FileProcessor("input.txt");
         FileProcessor nameProcessor = new FileProcessor("lang.txt");
         FileProcessor codeProcessor = new FileProcessor("langab.txt");
         String[] langCode = new String[50];
         String[] lang = new String[50];
         String outputFile="output.txt";
-
-        //outputFile = args[1].trim();
 
         int[] languageScore = new int[50];
         boolean[] latin = {false,true,false, true, true, true, true, true, true,
@@ -48,32 +47,36 @@ public class Driver {
         }
 
         String[] englishDictionary = new String[100];
+
         int counter=0;
-
         String line = null;
-
         while (((line = inputProcessor.readLineFromFile())!=null) && counter<100){
             englishDictionary[counter]=line;
             counter++;
         }
 
+        /**
+         * appending URL with . and then every word so that we can get
+         * meaning of every word differently
+         */
+
         counter=0;
-
         String[][] dictionary = new String[52][100];
-
         String back_url = " . " + englishDictionary[counter];
 
         for (int i=1; i<99; i++) {
             counter++;
-
             back_url += " . " + englishDictionary[counter];
         }
 
+        /**
+         * Selenium is an api, does job of calling browser -> and putting url
+         * and getting output words depending upon whether its latin or not.
+         */
         for (int b=0; b<lang.length; b++) {
             languageScore[b]=0;
             String url = "http://translate.google.com/#en/" + langCode[b] + "/" + back_url;
             counter=0;
-
             if (latin[b]==true) {
                 dictionary[b] = Selenium2Example.get2(url,driver);
             } else {
@@ -81,34 +84,20 @@ public class Driver {
             }
             System.out.println( lang[b] + " dictionary : " + dictionary[b].length );
         }
-
-
-        //BufferedWriter bw = null;
+        /**
+         * Comparing dictionary to every other dictionary letters.
+         * And matched frequency count is store in scoreArray and Writing that to file
+         */
         int count = 0;
-        //bw = new BufferedWriter(new FileWriter(outputFile, true));
         for (int x=0; x<50; x++) {
             for (int y=0; y<50; y++) {
                 count = 0;
                 for (int i=0; i<90; i++) {
                     scoreArray[x][y] = StringOperations.compareString(dictionary[x][i], dictionary[y][i]);
                     count += StringOperations.compareString(dictionary[x][i], dictionary[y][i]);
-
-                    //System.out.print("score : " + scoreArray[x][y]);
-                    //System.out.println(" Count : " + count);
-                     //inputProcessor.writeLineToFile(outputFile, scoreArray);
-                    //bw.write(String.valueOf(scoreArray[x][y]));
-                    //bw.write(String.valueOf(scoreArray[x][y] + " " + count));
-                    //bw.newLine();
                 }
                 inputProcessor.writeLineToFile(outputFile, scoreArray[x][y], count);
-                //bw.write(String.valueOf(scoreArray[x][y] + " " + count));
-                //bw.newLine();
             }
-
-
         }
-
-        //System.out.println("This is final output :" + Arrays.toString(scoreArray));
-        //bw.close();
     }
 }
